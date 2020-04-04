35a7aebfb19439acd032fa4873fa48efe96218e3
farbs03 <54757420+farbs03@users.noreply.github.com>
2020-04-03 21:27:54 -0400

    Add files via upload

---
+++ Personal_Website/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template('home.html')
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+@app.route("/projects")
+def projects():
+	return render_template("projects.html")
+
+@app.route("/useful_resources")
+def useful_resources():
+	return render_template("useful_resources.html")
+
+@app.route("/other")
+def other():
+	return render_template("other.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
